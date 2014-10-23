50c7962650f05c739a9bca3cea6fa7f500dd6424
Ariel Rodriguez <ariel@volonbolon.net>
2014-10-23 20:44:50 -0300

    Add color style sheet

---
+++ MiniatureOctoRobot/MiniatureOctoRobot/UIColor+Stylesheet.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Config.h
+//  MiniatureOctoRobot
+//
+//  Created by Ariel Rodriguez on 10/23/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Stylesheet)
++ (instancetype)navbarTintColor;
+@end
