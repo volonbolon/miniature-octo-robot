c2a0b2dd47c2d1f351eaa3c3500cd958472c5bd3
Ariel Rodriguez <ariel@volonbolon.net>
2014-10-23 21:19:41 -0300

    Add mainImage

--- MiniatureOctoRobot/MiniatureOctoRobot/UIImage+Stylesheet.m
+++ MiniatureOctoRobot/MiniatureOctoRobot/UIImage+Stylesheet.m
@@ -7,7 +7,27 @@
 //
 
 #import "UIImage+Stylesheet.h"
+#import "NSBundle+Config.h"
 
 @implementation UIImage (Stylesheet)
-
++ (instancetype)mainImage
+{
+    
+    __block UIImage *image = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        NSBundle *configBundle = [NSBundle configBundle];
+        NSDictionary *config = [configBundle config];
+        NSDictionary *images = [config objectForKey:@"images"];
+        NSString *filename = [images objectForKey:@"main_image_name"];
+        NSArray *components = [filename componentsSeparatedByString:@"."];
+        NSString *path = [configBundle pathForResource:[components firstObject] ofType:[components lastObject]];
+        image = [UIImage imageWithContentsOfFile:path];
+        
+    });
+    
+    return image;
+    
+}
 @end
